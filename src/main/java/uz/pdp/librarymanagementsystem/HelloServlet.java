001b19c4b5fec5d637d6d2e718b908a541bf6532
wrestling7877 <wrestling7877@gmail.com>
2022-08-05 22:51:37 +0500

    commit message

--- src/main/java/uz/pdp/librarymanagementsystem/HelloServlet.java
+++ src/main/java/uz/pdp/librarymanagementsystem/HelloServlet.java
@@ -24,6 +24,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        out.println("My name is Bekzod");
 
     }
 
